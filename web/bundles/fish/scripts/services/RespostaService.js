df74426e76fc99a5276b8623295d2076bcd5e934
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2015-04-30 19:25:25 -0300

    craida a funcao readAll do respostaService

--- web/bundles/fish/scripts/services/RespostaService.js
+++ web/bundles/fish/scripts/services/RespostaService.js
@@ -1,9 +1,9 @@
 app.factory('RespostaService', function ($http, BASE_URL) {
-    var respostas = [];
     var Resposta = {
         read: function () {
             return $http.get(BASE_URL + '/resposta');
-        }
+        },
+        readAll: function () { return $http.get(BASE_URL + '/resposta'); }
     };
     return Resposta;
 });
