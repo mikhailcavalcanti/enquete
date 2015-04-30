0a9ddf9268ddfcb38f53b03166674756b0b64c48
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2015-04-30 03:33:02 -0300

    concluido crud do servicepergunta do front

--- web/bundles/fish/scripts/services/PerguntaService.js
+++ web/bundles/fish/scripts/services/PerguntaService.js
@@ -1,8 +1,17 @@
 app.factory('PerguntaService', function ($http, BASE_URL) {
     var perguntas = [];
     var Pergunta = {
+        create: function (pergunta) {
+            return $http.post(BASE_URL + '/pergunta', pergunta);
+        },
         read: function () {
             return $http.get(BASE_URL + '/pergunta');
+        },
+        readAll: function() {
+            return $http.get(BASE_URL + '/pergunta');
+        },
+        delete: function (pergunta) {
+            return $http.delete(BASE_URL + '/pergunta/' + pergunta.id);
         }
     };
     return Pergunta;
